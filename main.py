c16b9465f9066ce5893d5755f924f3eb3f831c74
Heecheol <kimhc6028@gmail.com>
2017-06-08 18:19:46 +0900

    default epoch fixed

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ import model
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=100, metavar='N',
-                    help='number of epochs to train (default: 100)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
                     help='learning rate (default: 0.0001)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
