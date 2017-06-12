17d82b90034a67c3bc4be6c0842ead3aa855b58e
Heecheol Kim <kimhc6028@gmail.com>
2017-06-12 12:23:01 +0900

    changed to lighter model, with coordinates of object added

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import model
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
+parser.add_argument('--epochs', type=int, default=20, metavar='N',
+                    help='number of epochs to train (default: 20)')
 parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
                     help='learning rate (default: 0.0001)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
--- model.py
+++ model.py
@@ -1,55 +1,75 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import Variable
 
 class RN(nn.Module):
 
     def __init__(self,args):
         super(RN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
-        self.batchNorm1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
-        self.batchNorm2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
-        self.batchNorm3 = nn.BatchNorm2d(128)
-        self.conv4 = nn.Conv2d(128, 256, 3, stride=2, padding=1)
-        self.batchNorm4 = nn.BatchNorm2d(256)
+        self.conv1 = nn.Conv2d(3, 24, 3, stride=2, padding=1)
+        self.batchNorm1 = nn.BatchNorm2d(24)
+        self.conv2 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
+        self.batchNorm2 = nn.BatchNorm2d(24)
+        self.conv3 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
+        self.batchNorm3 = nn.BatchNorm2d(24)
+        self.conv4 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
+        self.batchNorm4 = nn.BatchNorm2d(24)
         
-        self.g_fc1 = nn.Linear(512+11, 2000)
-        self.g_fc2 = nn.Linear(2000, 2000)
-        self.g_fc3 = nn.Linear(2000, 2000)
-        self.g_fc4 = nn.Linear(2000, 2000)
+        self.g_fc1 = nn.Linear((24+2)*2+11, 256)
+        #self.g_fc1 = nn.Linear(24*5*5+11, 256)
+        self.g_fc2 = nn.Linear(256, 256)
+        self.g_fc3 = nn.Linear(256, 256)
+        self.g_fc4 = nn.Linear(256, 256)
 
-        self.f_fc1 = nn.Linear(2000, 1000)
-        self.f_fc2 = nn.Linear(1000, 500)
-        self.f_fc3 = nn.Linear(500, 100)
-        self.f_fc4 = nn.Linear(100, 10)
+        self.f_fc1 = nn.Linear(256, 256)
+        self.f_fc2 = nn.Linear(256, 256)
+        self.f_fc3 = nn.Linear(256, 10)
 
         self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
+
+        self.coord_oi = torch.FloatTensor(args.batch_size, 2)
+        self.coord_oj = torch.FloatTensor(args.batch_size, 2)
+        if args.cuda:
+            self.coord_oi = self.coord_oi.cuda()
+            self.coord_oj = self.coord_oj.cuda()
+        self.coord_oi = Variable(self.coord_oi)
+        self.coord_oj = Variable(self.coord_oj)
+
+        self.coord_lst = [torch.from_numpy(np.array([self.cvt_coord(i) for _ in range(args.batch_size)])) for i in range(25)]
+
+
+    def cvt_coord(self, i):
+        return [(i/5-2)/2., (i%5-2)/2.]
 
 
     def forward(self, img, qst):
         """convolution"""
         x = self.conv1(img)
+        x = F.relu(x)
         x = self.batchNorm1(x)
-        x = F.relu(x)
         x = self.conv2(x)
+        x = F.relu(x)
         x = self.batchNorm2(x)
-        x = F.relu(x)
         x = self.conv3(x)
+        x = F.relu(x)
         x = self.batchNorm3(x)
-        x = F.relu(x)
         x = self.conv4(x)
-        x = self.batchNorm4(x)
         x = F.relu(x)
+        x = self.batchNorm4(x)
         ## x = (64 x 256 x 5 x 5)
         """g"""
         x_g = 0
         for i in range(25):
             oi = x[:,:,i/5,i%5]
+            self.coord_oi.data.copy_(self.coord_lst[i])
+            oi = torch.cat((oi,self.coord_oi), 1)
             for j in range(25):
                 oj = x[:,:,j/5,j%5]
+                self.coord_oj.data.copy_(self.coord_lst[j])
+                oj = torch.cat((oj,self.coord_oj), 1)
                 x_ = torch.cat((oi,oj,qst), 1)
                 x_ = self.g_fc1(x_)
                 x_ = F.relu(x_)
@@ -60,15 +80,13 @@ class RN(nn.Module):
                 x_ = self.g_fc4(x_)
                 x_ = F.relu(x_)
                 x_g += x_
-
         """f"""
         x_f = self.f_fc1(x_g)
         x_f = F.relu(x_f)
         x_f = self.f_fc2(x_f)
         x_f = F.relu(x_f)
+        x_f = F.dropout(x_f)
         x_f = self.f_fc3(x_f)
-        x_f = F.relu(x_f)
-        x_f = self.f_fc4(x_f)
 
         return F.log_softmax(x_f)
 
