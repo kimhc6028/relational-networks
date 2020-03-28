fbc799ec6aab61162a3d236508f5f2463fbdacb9
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2020-03-28 13:05:59 +0100

    Add python command to run ternary RN model

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 python sort_of_clevr_generator.py
 
-python main.py --model=RN      --epochs=20
+python main.py --model=RN      --epochs=20      --relation-type=binary
+
+python main.py --model=RN      --epochs=20      --relation-type=ternary
 
 python main.py --model=CNN_MLP --epochs=100
